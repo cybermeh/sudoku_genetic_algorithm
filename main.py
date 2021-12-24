ca736cda863c1d18a88bc1f6d0e9c4f04f5528d1
cybermeh <cybermeh@icloud.com>
2021-12-24 02:54:05 +0100

    Initial

---
+++ main.py
@@ -0,0 +1,16 @@
+from sudoku import Sudoku
+
+if __name__ == '__main__':
+    s = Sudoku(
+        number_of_candidates=500,
+        number_of_elites=80,
+        number_of_generations=1000,
+        number_of_mutations=0
+    )
+
+    s.load('input.txt')
+
+    r = s.solve()
+
+    if r:
+        s.save('output.txt', r)
