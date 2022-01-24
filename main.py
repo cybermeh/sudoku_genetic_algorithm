9163210d9e5ef14fe8c4d101633c4cde16dd77da
cybermeh <cybermeh@icloud.com>
2022-01-24 11:50:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ if __name__ == '__main__':
         number_of_candidates=500,
         number_of_elites=80,
         number_of_generations=1000,
-        number_of_mutations=0
+        number_of_mutations=0,
+        selection_rate=0.85
     )
 
     s.load('input.txt')
